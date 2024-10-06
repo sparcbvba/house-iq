d7de8162c998ce45756da07b18bcc0bef2adb4a6
SPARC bvba <sparcbvba@users.noreply.github.com>
2024-10-06 14:35:03 +0200

    admin dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   plugins: [
     require('@tailwindcss/forms'),
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('flowbite/plugin')({
+      charts: true,
+    })
   ]
 }
